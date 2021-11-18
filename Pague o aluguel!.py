7d897ab77c11d6de245d728043622b113fbeec72
danoliveira14 <danielfrancelino1@hotmail.com>
2021-11-18 01:06:14 -0300

    reformulação

--- Pague o aluguel!.py
+++ Pague o aluguel!.py
@@ -1,23 +1,28 @@
+import math
+
 a_pagar = int(input())
 pago_por_mes = int(input())
 
 restante = 1
 parcelar = a_pagar / pago_por_mes
+math.ceil(parcelar)
 dps = a_pagar - pago_por_mes
+zero = 0
 
-if a_pagar < pago_por_mes:
-    zero = 0
-    print('pagamento:', restante)
-    print('antes = ', a_pagar)
-    print('depois =', zero)
-    print("-" * 5)
-else:
-    while parcelar != 0:
+
+if a_pagar > pago_por_mes:
+    while dps >= 0:
         print('pagamento:', restante)
-        print('antes = ', a_pagar)
+        print('antes =', a_pagar)
         print('depois =', dps)
         print("-" * 5)
-        parcelar -= 1
         a_pagar -= pago_por_mes
         restante += 1
         dps -= pago_por_mes
+        parcelar -= 1
+
+if parcelar != 0:
+    print('pagamento:', restante)
+    print('antes =', a_pagar)
+    print('depois =', zero)
+    print("-" * 5)
